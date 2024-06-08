b100473fa90e732c6fbe0a8f81bb1e208fa4f22f
the_last_naik <sodaisa_4tsuki@hotmail.com>
2024-06-08 17:50:05 +0530

    Changes Commit

--- src/programs/SumSubArray.java
+++ src/programs/SumSubArray.java
@@ -1,27 +1,50 @@
 package programs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SumSubArray {
     public static boolean checkSubarraySum(int[] nums, int k){
-        if(nums.length > 2) {
-            int sum = 0;
-            int size = nums.length;
-            for (int i : nums) {
-                sum = sum + i;
-            }
 
-            if ((sum % k) == 0) {
-                int result = sum % k;
-                return result == 0;
-            }
+        for(int i = 0; i<= nums.length - 1; i++){
+            List<Integer> subarray = new ArrayList<>();
+            int sum = nums[i];
+            subarray.add(nums[i]);
+            for(int j = i + 1; j< nums.length; j++){
+                subarray.add(nums[j]);
+                sum = sum + nums[j];
 
-            return false;
+                    if ((sum % k) == 0) {
+                        int result = sum % k;
+                        return result == 0;
+                    }
+            }
         }
         return false;
     }
 
     public static void main(String[] args) {
-        int[] nums = {23,2,4,6,6};
+        int[] nums = {23,2,6,4,7};
 
-        System.out.println("Result is " +checkSubarraySum(nums, 7));
+        System.out.println("Result is " +checkSubarraySum(nums, 13));
+    }
+
+    public static boolean checkSubArray(int[] nums, int k){
+        if(nums.length > 2) {
+            int sum = 0;
+            List<Integer> subArray = new ArrayList<>();
+            for (int i : nums) {
+                sum = sum + i;
+                subArray.add(i);
+                if(subArray.size() > 2){
+                    if ((sum % k) == 0) {
+                        int result = sum % k;
+                        return result == 0;
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
     }
 }
